24cd6da1f19e440e3d3b192da70a8772e7cb912b
liusunan <liusunan@aidigger.com>
2020-02-25 13:19:47 +0800

    ignore

--- examples/bert_crf/run.sh
+++ examples/bert_crf/run.sh
@@ -7,16 +7,6 @@ export SAVE_STEPS=750
 export SEED=1
 export DATA_DIR=PATH_TO_DATA
 
-
-export CUDA_VISIBLE_DEVICES=0
-export MAX_LENGTH=128
-export BERT_MODEL=bert-base-chinese
-export BATCH_SIZE=8
-export NUM_EPOCHS=3
-export SAVE_STEPS=750
-export SEED=1
-export DATA_DIR=PATH_TO_DATA
-
 #`crf&init_transitions&multi_decode train&eval
 python3 run_bert_crf.py --data_dir $DATA_DIR \
 --model_type bert \
